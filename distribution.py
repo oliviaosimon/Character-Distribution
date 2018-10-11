f2ba41f09a81b5a3eec651a20a6ffdb9c622ff2e
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-10-10 23:30:00 -0400

    Updated from Brython Server: ‎10‎/‎10‎/‎2018 ‎11‎:‎30‎:‎01‎ ‎PM

--- distribution.py
+++ distribution.py
@@ -42,43 +42,9 @@ text = input("Please enter a string of text (the bigger the better): ").lower()
 print('The distribution of characters in "'+text+'" is:')
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#def sort_list(list1, list2): 
-  
-    #zipped_pairs = zip(list2, list1) 
-  
-    #z = [x for _, x in sorted(zipped_pairs, reverse=True)] 
-      
-    #return z 
 
-#Variables
 
-a = text.count('a')
-b = text.count('b')
-c = text.count('c')
-d = text.count('d')
-e = text.count('e')
-f = text.count('f')
-g = text.count('g')
-h = text.count('h')
-i = text.count('i')
-j = text.count('j')
-k = text.count('k')
-l = text.count('l')
-m = text.count('m')
-n = text.count('n')
-o = text.count('o')
-p = text.count('p')
-q = text.count('q')
-r = text.count('r')
-s = text.count('s')
-t = text.count('t')
-u = text.count('u')
-v = text.count('v')
-w = text.count('w')
-x = text.count('x')
-y = text.count('y')
-z = text.count('z')
-
+text = list(text)
 #variable amounts
 alphaNum = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
 
@@ -86,7 +52,7 @@ printingList = list()
 for x in alpha:
     while text.count(x) > 0:
         distribution = ((text.count(x))*x)
-    printingList.append(x)
+    printingList.append(distribution)
 
 
 print(printingList)
